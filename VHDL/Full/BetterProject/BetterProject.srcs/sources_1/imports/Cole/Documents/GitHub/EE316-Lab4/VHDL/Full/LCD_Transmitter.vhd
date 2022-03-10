15915c4a7e4a434c8df9bbab1fb7afe20344962b
ColeCaruso <99911268+ColeCaruso@users.noreply.github.com>
2022-03-10 15:50:09 -0500

    top_level

--- VHDL/Full/BetterProject/BetterProject.srcs/sources_1/imports/Cole/Documents/GitHub/EE316-Lab4/VHDL/Full/LCD_Transmitter.vhd
+++ VHDL/Full/BetterProject/BetterProject.srcs/sources_1/imports/Cole/Documents/GitHub/EE316-Lab4/VHDL/Full/LCD_Transmitter.vhd
@@ -8,9 +8,10 @@ ENTITY LCD_Transmitter IS
 	PORT(
 		clk       : IN    STD_LOGIC;                     --system clock
 		reset_n   : IN    STD_LOGIC;
-		
-		run_clk   : IN    STD_LOGIC;  --is the clock on
-		adc_sel   : IN    STD_LOGIC_VECTOR(1 DOWNTO 0); 
+        indata	  : IN    STD_LOGIC_VECTOR(255 downto 0);
+        
+--		run_clk   : IN    STD_LOGIC;  --is the clock on
+--		adc_sel   : IN    STD_LOGIC_VECTOR(1 DOWNTO 0);
 		
 		sda       : inout std_logic;                     --i2c data
 		scl       : inout std_logic                      --i2c clock
@@ -32,13 +33,13 @@ component i2c_master is
 		data_wr   : IN     STD_LOGIC_VECTOR(7 DOWNTO 0); --data to write to slave
 		busy      : OUT    STD_LOGIC;                    --indicates transaction in progress
 		data_rd   : OUT    STD_LOGIC_VECTOR(7 DOWNTO 0); --data read from slave
-		ack_error : BUFFER STD_LOGIC;                    --flag if improper acknowledge from slave
+		ack_error : BUFFER STD_LOGIC;                  --flag if improper acknowledge from slave
 		sda       : INOUT  STD_LOGIC;                    --serial data output of i2c bus
 		scl       : INOUT  STD_LOGIC);                   --serial clock output of i2c bus
 end component;
 
 --LCD Arrays
-type LCD_FirstLine is array(0 to 3) of std_logic_vector(127 downto 0);
+type LCD_FirstLine is array(0 to 3) of std_logic_vector(255 downto 128);
 signal first_line : LCD_FirstLine := (others => (others => '0'));
 TYPE LCD_SecondLine is array(0 to 1) of std_logic_vector(127 downto 0);
 signal second_line : LCD_SecondLine := (others => (others => '0'));
@@ -91,18 +92,14 @@ Inst_i2c_master : i2c_master
 -- DATA PREPERATION --
 -- First Line  --
 -- This part is editable for custom messages 
---                     	                               A       0        :              L       D       R
-first_line(0)     <= x"20" & x"20" & x"20" & x"20" & x"41" & x"30" & x"3a" & x"20" & x"4c" & x"44" & x"52" & x"20" & x"20" & x"20" & x"20" & x"20";
---                     	                               A       1        :              T       E       M        P
-first_line(1)     <= x"20" & x"20" & x"20" & x"20" & x"41" & x"31" & x"3a" & x"20" & x"54" & x"45" & x"4d" & x"50" & x"20" & x"20" & x"20" & x"20";
---                     	                        A       2        :       A      N        A       L       O       G    
-first_line(2)     <= x"20" & x"20" & x"20" & x"41" & x"32" & x"3a" & x"41" & x"4e" & x"41" & x"4c" & x"4f" & x"47" & x"20" & x"20" & x"20" & x"20";
---                     	                               A       3        :               P       O       T       
-first_line(3)     <= x"20" & x"20" & x"20" & x"20" & x"41" & x"33" & x"3a" & x"20" & x"50" & x"4f" & x"54" & x"20" & x"20" & x"20" & x"20" & x"20";
+--                 
+first_line(0)     <= indata(127 downto 0);
+--           
+
 --
-second_line(0)    <= x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20" & x"20";
---                                              C      L       O        C       K              G        E      N
-second_line(1)    <= x"20" & x"20" & x"20" & x"43" & x"4c" & x"4f" & x"43" & x"4B" & x"20" & x"47" & x"45" & x"4e" & x"20" & x"20" & x"20" & x"20";
+second_line(0)    <= indata(255 downto 128);
+--                               
+
  
 
 process(byteSel)
@@ -116,39 +113,39 @@ process(byteSel)
        when 5  => data  <= '0'& X"0F";
        when 6  => data  <= '0' & X"0C";
        when 7  => data  <= '0' & X"80";
-       when 8 => data   <= '1'& first_line(to_integer(unsigned(adc_sel)))(127 downto 120);
-       when 9 => data   <= '1'& first_line(to_integer(unsigned(adc_sel)))(119 downto 112);
-       when 10 => data   <= '1'& first_line(to_integer(unsigned(adc_sel)))(111 downto 104);
-       when 11 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(103 downto  96);
-       when 12 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(95  downto  88);
-       when 13 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(87  downto  80);
-       when 14 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(79  downto  72); 
-       when 15 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(71  downto  64);
-       when 16=> data   <= '1'& first_line(to_integer(unsigned(adc_sel)))(63  downto  56);
-       when 17 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(55  downto  48);
-       when 18 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(47  downto  40);
-       when 19 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(39  downto  32);
-       when 20 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(31  downto  24);
-       when 21 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(23  downto  16);
-       when 22 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(15  downto   8);
-       when 23 => data  <= '1'& first_line(to_integer(unsigned(adc_sel)))(7   downto   0);
+       when 8 => data   <= '1'& first_line(0)(127 downto 120);
+       when 9 => data   <= '1' & first_line(0)(119 downto 112);
+       when 10 => data   <= '1'& first_line(0)(111 downto 104);
+       when 11 => data  <= '1'& first_line(0)(103 downto  96);
+       when 12 => data  <= '1'& first_line(0)(95  downto  88);
+       when 13 => data  <= '1'& first_line(0)(87  downto  80);
+       when 14 => data  <= '1'& first_line(0)(79  downto  72); 
+       when 15 => data  <= '1'& first_line(0)(71  downto  64);
+       when 16=> data   <= '1'& first_line(0)(63  downto  56);
+       when 17 => data  <= '1'& first_line(0)(55  downto  48);
+       when 18 => data  <= '1'& first_line(0)(47  downto  40);
+       when 19 => data  <= '1'& first_line(0)(39  downto  32);
+       when 20 => data  <= '1'& first_line(0)(31  downto  24);
+       when 21 => data  <= '1'& first_line(0)(23  downto  16);
+       when 22 => data  <= '1'& first_line(0)(15  downto   8);
+       when 23 => data  <= '1'& first_line(0)(7   downto   0);
        when 24 => data  <= '0'& X"C0";--Change address to bottom left of screen--
-       when 25 => data  <= '1'& second_line(std_to_integer(run_clk))(127 downto 120);
-       when 26 => data  <= '1'& second_line(std_to_integer(run_clk))(119 downto 112);
-       when 27 => data  <= '1'& second_line(std_to_integer(run_clk))(111 downto 104);
-       when 28 => data  <= '1'& second_line(std_to_integer(run_clk))(103 downto  96);
-       when 29 => data  <= '1'& second_line(std_to_integer(run_clk))(95  downto  88);
-       when 30 => data  <= '1'& second_line(std_to_integer(run_clk))(87  downto  80);
-       when 31 => data  <= '1'& second_line(std_to_integer(run_clk))(79  downto  72); 
-       when 32 => data  <= '1'& second_line(std_to_integer(run_clk))(71  downto  64);
-       when 33 => data  <= '1'& second_line(std_to_integer(run_clk))(63  downto  56);
-       when 34 => data  <= '1'& second_line(std_to_integer(run_clk))(55  downto  48);
-       when 35 => data  <= '1'& second_line(std_to_integer(run_clk))(47  downto  40);
-       when 36 => data  <= '1'& second_line(std_to_integer(run_clk))(39  downto  32);
-       when 37 => data  <= '1'& second_line(std_to_integer(run_clk))(31  downto  24);
-       when 38 => data  <= '1'& second_line(std_to_integer(run_clk))(23  downto  16);
-       when 39 => data  <= '1'& second_line(std_to_integer(run_clk))(15  downto   8);
-       when 40 => data  <= '1'& second_line(std_to_integer(run_clk))(7   downto   0);
+       when 25 => data  <= '1'& second_line(0)(127 downto 120);
+       when 26 => data  <= '1'& second_line(0)(119 downto 112);
+       when 27 => data  <= '1'& second_line(0)(111 downto 104);
+       when 28 => data  <= '1'& second_line(0)(103 downto  96);
+       when 29 => data  <= '1'& second_line(0)(95  downto  88);
+       when 30 => data  <= '1'& second_line(0)(87  downto  80);
+       when 31 => data  <= '1'& second_line(0)(79  downto  72); 
+       when 32 => data  <= '1'& second_line(0)(71  downto  64);
+       when 33 => data  <= '1'& second_line(0)(63  downto  56);
+       when 34 => data  <= '1'& second_line(0)(55  downto  48);
+       when 35 => data  <= '1'& second_line(0)(47  downto  40);
+       when 36 => data  <= '1'& second_line(0)(39  downto  32);
+       when 37 => data  <= '1'& second_line(0)(31  downto  24);
+       when 38 => data  <= '1'& second_line(0)(23  downto  16);
+       when 39 => data  <= '1'& second_line(0)(15  downto   8);
+       when 40 => data  <= '1'& second_line(0)(7   downto   0);
        when others => data <= '0'& X"20"; 
    end case;
 end process;
